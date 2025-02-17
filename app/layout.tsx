057456062497c4a85d32cef7c95a41edfb6ff27f
Petrpejsek <petrliesner@gmail.com>
2025-02-17 13:19:59 +0100

    Odstranění duplicitního headeru (Navbar)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import './globals.css'
 import { Providers } from './providers'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -36,7 +35,6 @@ export default function RootLayout({
         <Providers>
           <div className="min-h-screen flex flex-col">
             <Header />
-            <Navbar />
             <main className="flex-grow">
               {children}
             </main>
