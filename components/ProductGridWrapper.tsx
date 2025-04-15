c87f43f6ffb2505ebd76b8a9e8f103438a2e54ea
Petrpejsek <petrliesner@gmail.com>
2025-04-15 15:05:15 +0200

    Oprava použití komponent TagFilter a ProductGrid

--- components/ProductGridWrapper.tsx
+++ components/ProductGridWrapper.tsx
@@ -1,16 +1,14 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 import ProductGrid from './ProductGrid'
 import TagFilter from './TagFilter'
 
 export default function ProductGridWrapper() {
-  const [selectedTags, setSelectedTags] = useState<Set<string>>(new Set())
-
   return (
     <>
-      <TagFilter selectedTags={selectedTags} onTagsChange={setSelectedTags} />
-      <ProductGrid selectedTags={selectedTags} />
+      <TagFilter />
+      <ProductGrid />
     </>
   )
 } 
